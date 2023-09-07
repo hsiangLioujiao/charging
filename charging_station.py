92bdbf0d9f0c10260be4518a130d479ff553a99d
hsiangLioujiao <99463623+hsiangLioujiao@users.noreply.github.com>
2023-09-07 13:51:11 +0800

    Update charging_station.py

--- charging_station.py
+++ charging_station.py
@@ -5,7 +5,7 @@ from PIL import Image
 
 st.title('充電站')
 st.subheader("資料來源：")
-image = Image.open('d:\\code\\2023\\jpgs\\open_data.PNG')
+image = Image.open('open_data.PNG')
 st.image(image)
 st.divider()
 
